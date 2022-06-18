424d7ac9c979cdbbd9ddb04497b0a630ffe03b32
LonerStayle <dlwlstjq410@gmail.com>
2022-06-18 14:41:55 +0900

    [Refactor] KeyUseCase.kt 위치 변경

---
+++ service_locator/src/main/java/kr/loner/service_locator/KeyUseCase.kt
@@ -0,0 +1,9 @@
+package kr.loner.service_locator
+
+enum class KeyUseCase {
+    CREATE_SCHEDULE_USE_CASE,
+    DELETE_SCHEDULE_USE_CASE,
+    GET_SCHEDULE_GROUP_USE_CASE,
+    GET_SCHEDULE_USE_CASE,
+    UP_DATE_SCHEDULE_USE_CASE,
+}
--- service_locator/src/main/java/kr/loner/service_locator/ServiceLocator.kt
+++ service_locator/src/main/java/kr/loner/service_locator/ServiceLocator.kt
@@ -49,12 +49,6 @@ object ServiceLocator {
     }
 
 
-    enum class KeyUseCase {
-        CREATE_SCHEDULE_USE_CASE,
-        DELETE_SCHEDULE_USE_CASE,
-        GET_SCHEDULE_GROUP_USE_CASE,
-        GET_SCHEDULE_USE_CASE,
-        UP_DATE_SCHEDULE_USE_CASE,
-    }
+
 
 }
